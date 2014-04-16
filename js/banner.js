b672a0f60c36291ddf2951ce69ff67407bd59291
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-16 20:00:03 +0800

    Fixes

--- js/banner.js
+++ js/banner.js
@@ -76,7 +76,12 @@ $(function(){
 		$(".tablet_control .bullet.enabled .time_limit").css("width", "12.5%");
 	});
 	
-	banner_interval = setInterval(function(){rotate_banner( total_banner )},1000);
+	if ( total_banner>1 ) banner_interval = setInterval(function(){rotate_banner( total_banner )},1000);
+	else {
+		$(".tablet_control").css('display','none');
+		$(".banner_arrow").css('display','none');
+		$(".middle-banner").css('margin-top','10px');
+	}
 	
 	$(".tablet_control .control_button .stop").click(function(){
 		clearInterval( banner_interval );
